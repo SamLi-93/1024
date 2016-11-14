3d034222f9aaf925726b4011214b46da062d7164
Sam <342447974@qq.com>
2016-11-14 21:09:10 +0800

    1

--- porn_spider/spiders/porn_cl_spider.py
+++ porn_spider/spiders/porn_cl_spider.py
@@ -18,7 +18,7 @@ class DoubanSpider(CrawlSpider):
     ]
 
     rules = (
-        Rule(LinkExtractor(allow=(r'http://cl.miicool.info/thread0806.php\?fid=22&search=&page=[1-10]')),
+        Rule(LinkExtractor(allow=(r'http://cl.miicool.info/thread0806.php\?fid=22&search=&page=[1-9]')),
              callback='parse_item',
              follow=True),
     )
